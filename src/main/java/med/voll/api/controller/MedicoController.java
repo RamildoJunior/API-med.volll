7b8fbeaea50e26b4ecff422b95d21a35cad009f2
Ramildo Pereira da Silva Junior <rpsjuninho@gmail.com>
2023-06-28 14:11:51 -0300

    realizando listagem de medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,18 +1,33 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.CadastroMedico;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.medico.ListagemMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
-    @PostMapping
-    public void cadastrar(@RequestBody CadastroMedico dados) {
-        System.out.println(dados);
+    @Autowired
+    private MedicoRepository repository;
 
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid CadastroMedico dados) {
+       repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<ListagemMedico> listar(){
+        return repository.findAll().stream().map(ListagemMedico::new).toList();
     }
 }
 
